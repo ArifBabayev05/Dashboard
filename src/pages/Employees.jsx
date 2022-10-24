368832664056468874c3c42807d855763bc8363b
Arif Babayev <arifrb@code.edu.az>
2022-10-24 19:44:25 +0400

    Add Persons Table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,41 +1,117 @@
-import React from "react";
-import {
-  GridComponent,
-  ColumnsDirective,
-  ColumnDirective,
-  Page,
-  Inject,
-  Search,
-  Toolbar,
-} from "@syncfusion/ej2-react-grids";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Card from './EmployeesCard'
 
-import { employeesGrid, employeesData } from "../data/dummy";
-import { Header } from "../components";
 
-const Employees = () => {
-  return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg">
-      <Header title="Leads" />
-      <GridComponent
-        dataSource={employeesData}
-        allowPaging
-        title="T"
-        allowSorting
-        toolbar={["Search"]}
-        width="auto"
+function Employees() {
+  const url = `http://localhost:53410/api/Vacancies/getall`
+  const [query, setQuery] = useState("")
+
+
+  const [products, setProducts] = useState({
+    loading: false,
+    data: null,
+    error: false
+  })
+
+
+
+  useEffect(() => {
+    setProducts({
+      loading: true,
+      data: null,
+      error: false
+
+    })
+
+    axios.get(url)
+      .then(response => {
+        setProducts({
+          loading: false,
+          data: response.data,
+          error: false
+        })
+
+          .catch(() => {
+            setProducts({
+              loading: false,
+              data: null,
+              error: true
+            })
+          })
+      })
+  }, [url])
+
+  let content = null
+  
+
+
+  // if (products.loading) {
+  //   content = <Loader />
+  // }
+
+  if (products.error) {
+    content = <p>Xəta baş verdi, yenidən yoxlayın.</p>
+  }
+
+  if (products.data) {
+    content =
+      products.data.map((product) =>
+        <div key={product.id}>
+          <Card product={product} />
+        </div>
+      )
+
+  }
+
+
+  if (products.data) {
+    
+    content =
+      products.data.filter(product => {
+        if (query === "") {
+
+          return product;
+
+        } else if (product.name.toLowerCase().includes(query.toLowerCase())) {
+
+          return product;
+        }
+        else if (product.company.name.toLowerCase().includes(query.toLowerCase())) {
+
+          return product;
+        }
         
+        return "";
+      }).map((product) =>
+        <div key={product.id}>
+          <Card product={product} />
+        </div>
+      ).reverse();
+  }
 
-       >
-        <ColumnsDirective>
-          {employeesGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
-          ))}
-        </ColumnsDirective>
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg'>
+      <div className='d-flex container '>
+        <h1 className='text container ' style={{ color: 'var(--pink)', fontWeight: 'normal', fontSize: '35px', alignItems: 'center', display: 'flex' }}>Vakansiyalar</h1>
+        <form className='searchJob mb-5' style={{ alignItems: 'center', display: 'flex', top: '20px' }}>
+          <input className='searchBar mt-2 p-2' onChange={event => setQuery(event.target.value)} type='text'></input>
+          <button className='search__submit' type='submit'>
+            
+          </button>
+          {/* <button onClick={()=>sorting("salary")}>Maaşa görə sırala</button> */}
+        </form>
+      </div>
 
-        <Inject services={[Page, Search, Toolbar]} />
-      </GridComponent>
+      {content}
+
     </div>
-  );
-};
+  )
+}
 
-export default Employees;
+export default Employees
+
+
+
+
+
